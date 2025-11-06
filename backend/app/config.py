353f9f6c243d4d11c920f8e4f56e6ef0502c60d7
Hisui <i@hisui.io>
2025-11-06 17:12:09 +0800

    Initial commit

---
+++ backend/app/config.py
@@ -0,0 +1,23 @@
+import os
+from functools import lru_cache
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    # API Configuration
+    api_host: str = os.getenv("API_HOST", "0.0.0.0")
+    api_port: int = int(os.getenv("API_PORT", 8000))
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
---
+++ frontend/app/LoginRegister/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./LoginRegister.module.css"
+export default function SignUp(){
+    return (
+        <main className={styles.mainPage}>
+            <p> This is s a sample text</p>
+        </main>
+    )
+}
