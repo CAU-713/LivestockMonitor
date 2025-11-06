69d4de507285f0bebd0d2880e0b4ebc45c14aeaf
houzhuoyan <1910540703@qq.com>
2025-11-06 19:04:12 +0800

    转移路由相关代码

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,18 +2,11 @@ from typing import Union
 from app.config import settings
 import uvicorn
 from fastapi import FastAPI
+from .routers import test
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(test.router)
 
 
 if __name__ == "__main__":
