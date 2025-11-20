bdd8338c8f014acd2ee25d7f0114d196c4342294
Hisui <i@hisui.io>
2025-11-20 15:10:31 +0800

    feat(pages): add Historical Environmental and Video Data pages

---
+++ frontend/app/(main)/history/environmental-data/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HistoricalEnvironmentalDataPage = () => {
+  return (
+    <div>
+      <h1>Historical Environmental Data</h1>
+      {/* Content for historical environmental data goes here */}
+    </div>
+  );
+};
+
+export default HistoricalEnvironmentalDataPage;
---
+++ frontend/app/(main)/history/video-data/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HistoricalVideoDataPage = () => {
+  return (
+    <div>
+      <h1>Historical Video Data</h1>
+      {/* Content for historical video data goes here */}
+    </div>
+  );
+};
+
+export default HistoricalVideoDataPage;
